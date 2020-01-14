ea4ddec5951d46ec58267e9ff184d748707a0a87
zhanghongtong <zhanghongtong@foxmail.com>
2020-01-14 13:55:51 +0800

    Fix error for workflows

--- .github/workflows/script/build.sh
+++ .github/workflows/script/build.sh
@@ -41,12 +41,12 @@ test_pkg(){
             ;;
             "rpm")
             rpm -ivh $var
-            if [ $(rpm -q emqtt) != emqtt ];then
+            if [ $(rpm -q emqtt) != "emqtt*" ];then
                 echo 'package install error' && exit 1
             fi
             emqtt pub -t 'hello' --payload 'hello world' -h broker.emqx.io
             rpm -e emqtt
-            if [ $(rpm -q emqtt) == emqtt ];then
+            if [ $(rpm -q emqtt) == "emqtt*" ];then
                 echo 'package uninstall error' && exit 1
             fi
             ;;
