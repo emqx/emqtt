af7dc46e4050c06de16ddb5f2d9c217789a598f0
JianBo He <heeejianbo@gmail.com>
2022-08-23 12:57:04 +0800

    test: add eunit tests for emqtt_inflight

--- src/emqtt_inflight.erl
+++ src/emqtt_inflight.erl
@@ -18,6 +18,10 @@
 
 -include("emqtt.hrl").
 
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
 -export([ new/1
         , insert/3
         , update/3
@@ -70,7 +74,7 @@ update(Id, Req, Inflight = #{sent := Sent}) ->
             Inflight#{sent := maps:put(Id, {No, Req}, Sent)}
     end.
 
--spec(delete(id(), inflight()) -> error | inflight()).
+-spec(delete(id(), inflight()) -> error | {req(), inflight()}).
 delete(Id, Inflight = #{sent := Sent}) ->
     case maps:take(Id, Sent) of
         error -> error;
@@ -89,9 +93,10 @@ is_full(#{max_inflight := Max, sent := Sent}) ->
     maps:size(Sent) >= Max.
 
 -spec(is_empty(inflight()) -> boolean()).
+is_empty(#{max_inflight := infinity}) ->
+    false;
 is_empty(#{sent := Sent}) ->
     maps:size(Sent) =< 0.
-
 
 %% @doc first in first evaluate
 -spec(foreach(F, inflight()) -> ok when
@@ -122,3 +127,65 @@ sort_sent(Sent) ->
                    SeqNo1 < SeqNo2
            end,
     lists:sort(Sort, maps:to_list(Sent)).
+
+%%--------------------------------------------------------------------
+%% tests
+
+-ifdef(TEST).
+
+insert_delete_test() ->
+    Inflight = emqtt_inflight:new(2),
+    Inflight1 = emqtt_inflight:insert(1, req1, Inflight),
+    Inflight2 = emqtt_inflight:insert(2, req2, Inflight1),
+    error = emqtt_inflight:insert(3, req3, Inflight2),
+    error = emqtt_inflight:delete(3, Inflight),
+    {req2, _} = emqtt_inflight:delete(2, Inflight2).
+
+update_test() ->
+    Inflight = emqtt_inflight:new(2),
+    Inflight1 = emqtt_inflight:insert(1, req1, Inflight),
+    error = emqtt_inflight:update(2, req2, Inflight1),
+
+    Inflight11 = emqtt_inflight:update(1, req11, Inflight1),
+    {req11, _} = emqtt_inflight:delete(1, Inflight11).
+
+size_full_empty_test() ->
+    Inflight = emqtt_inflight:new(1),
+    0 = emqtt_inflight:size(Inflight),
+    true = emqtt_inflight:is_empty(Inflight),
+    false = emqtt_inflight:is_full(Inflight),
+
+    Inflight1 = emqtt_inflight:insert(1, req1, Inflight),
+    1 = emqtt_inflight:size(Inflight1),
+    false = emqtt_inflight:is_empty(Inflight1),
+    true = emqtt_inflight:is_full(Inflight1),
+
+    false = emqtt_inflight:is_full(emqtt_inflight:new(infinity)),
+    false = emqtt_inflight:is_empty(emqtt_inflight:new(infinity)).
+
+foreach_test() ->
+    emqtt_inflight:foreach(
+      fun(Id, Req) ->
+        true = (Id =:= Req)
+      end, inflight_example()).
+
+retry_test() ->
+    [{"sorted by insert sequence",
+      [{1, 1}, {2, 2}] = emqtt_inflight:retry(
+                           fun(Id, Req) -> Id =:= Req end,
+                           inflight_example()
+                          )
+     },
+     {"filter",
+      [{2, 2}] = emqtt_inflight:retry(
+                   fun(Id, _Req) -> Id =:= 2 end,
+                   inflight_example())
+     }].
+
+inflight_example() ->
+    emqtt_inflight:insert(
+      2, 2,
+      emqtt_inflight:insert(1, 1, emqtt_inflight:new(infinity))
+     ).
+
+-endif.
